643faa7144d2bccce572b68e7de33385be0baead
liyingang <1209995335@qq.com>
2019-10-22 09:37:35 +0800

    first commit

---
+++ src/test/java/com/jit/csi/CsiAudioPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jit.csi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsiAudioPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
